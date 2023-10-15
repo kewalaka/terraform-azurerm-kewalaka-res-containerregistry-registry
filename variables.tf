03df0522a67f6c484eb2c833f4f59bcabed6137d
kewalaka <kewalaka@gmail.com>
2023-10-16 12:50:04 +1300

    chore/fix naming convention & remove unnecessary telemetry parameter in examples

--- variables.tf
+++ variables.tf
@@ -18,8 +18,8 @@ variable "name" {
   type        = string
   description = "The name of the Container Registry."
   validation {
-    condition     = can(regex("^[a-z0-9-]{5,50}$", var.name))
-    error_message = "The name must be between 5 and 50 characters long and can only contain lowercase letters, numbers and dashes."
+    condition     = can(regex("^[a-z0-9]{5,50}$", var.name))
+    error_message = "The name must be between 5 and 50 characters long and can only contain lowercase letters and numbers."
   }
 }
 
