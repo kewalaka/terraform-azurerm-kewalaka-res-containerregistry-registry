5cd17f104f5e1a051614f0397fdb7d2e033ea597
kewalaka <kewalaka@gmail.com>
2023-10-30 18:36:14 +1300

    fix/make location optional and fix privatelink dns zone in example

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
+  location                           = var.location != null ? var.location : data.azurerm_resource_group.rg.location
 }
 
 # Private endpoint application security group associations
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_container_registry" "this" {
   name                          = var.name
   resource_group_name           = var.resource_group_name
